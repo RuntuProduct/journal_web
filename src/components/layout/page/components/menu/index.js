df4b5c8147026d76a29836bff29ad93b01af8792
runtu <huorunquan@126.com>
2018-09-20 14:13:53 +0800

    IMP: 完成标志视图

--- src/components/layout/page/components/menu/index.js
+++ src/components/layout/page/components/menu/index.js
@@ -1,8 +1,14 @@
 import les from './index.less'
+import Mark from './components/mark'
 
 const Menus = () => {
   return (
-    <div className={les.menuCon}>menus</div>
+    <div className={les.menuCon}>
+      {/* 标题 */}
+      <Mark />
+      {/* 用户信息 */}
+      {/* 菜单 */}
+    </div>
   )
 }
 
